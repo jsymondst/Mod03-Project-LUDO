b5e98d07f556a2cb12e296e0d4e6020887e46d87
github username <github email address>
2020-06-19 08:02:29 +0100

    player card and text styling

--- ludo-backend/app/models/player.rb
+++ ludo-backend/app/models/player.rb
@@ -4,7 +4,7 @@ class Player < ApplicationRecord
     has_many :games, through: :game_records
 
 
-    validates :username, uniqueness: true
+    # validates :username, uniqueness: true
     validates :username, presence: true
 
 end
--- ludo-backend/db/seeds.rb
+++ ludo-backend/db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Player.destroy_all
+Game.destroy_all
+GameRecord.destroy_all
 
 jamie = Player.create(
 
